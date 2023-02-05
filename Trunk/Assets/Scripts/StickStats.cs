0abc17227b564e89c4c50baeb9eb3c53ee117bbc
Bjuz <jeaustin.rdz@.com>
2023-02-04 22:33:40 -0600

    '

--- Trunk/Assets/Scripts/StickStats.cs
+++ Trunk/Assets/Scripts/StickStats.cs
@@ -70,11 +70,7 @@ public class StickStats : MonoBehaviour
     void LevelUPStick()
     {
 
-<<<<<<< HEAD
-        
-=======
        
->>>>>>> 045a416d92aae3e22fb149ab82fca60a4a5d709a
     }
     public void UpgradeWater(float newWater)
     {
